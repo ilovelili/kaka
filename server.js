cff247d06c7b9a36e235b100aba84f7690701814
ilovelili <route666@live.cn>
2020-06-02 00:29:34 +0900

    uport connect

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const ngrok = require("ngrok");
+const path = require("path");
+
+//setup boilerplate
+let endpoint = "";
+const app = express();
+
+app.get("/create", function (req, res) {
+	// res.sendFile(path.join(__dirname + "/uport.html"));
+	res.sendFile(path.join(__dirname + "/uport-create-verification.html"));
+});
+
+app.get("/request", function (req, res) {
+	// res.sendFile(path.join(__dirname + "/uport.html"));
+	res.sendFile(path.join(__dirname + "/uport-request-verification.html"));
+});
+
+app.use(express.static("lib"));
+
+// run the app server and tunneling service
+const server = app.listen(8088, () => {
+	ngrok.connect(8088).then((ngrokUrl) => {
+		endpoint = ngrokUrl;
+		console.log(`Your dApp is being served!, open at ${endpoint} and scan the QR to login!`);
+	});
+});
